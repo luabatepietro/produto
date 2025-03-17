8549117e43c856a65316fd2e3bff467aa4053961
luabatepietro <luabatepietroapple@icloud.com>
2025-03-17 11:24:43 -0300

    produtos teste

--- src/main/java/br/insper/produto/produto/Produto.java
+++ src/main/java/br/insper/produto/produto/Produto.java
@@ -1,33 +1,70 @@
 package br.insper.produto.produto;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "produtos")
 public class Produto {
+
     private String id;
     private String nome;
-    private Float preco;
-    private Integer estoque;
+    private double preco;
+    private int quantidadeEstoque;
 
-    public Produto(String nome, float valor){
+    public Produto(String id, String nome, double preco, int quantidadeEstoque) {
+        this.id = id;
         this.nome = nome;
-        this.preco = valor;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
     }
 
-    public Produto(){
-
+    public Produto() {
     }
 
-    public String getId() { return id; }
+    public String getId() {
+        return id;
+    }
 
-    public void setId(String id) { this.id = id; }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public String getNome() { return nome; }
+    public String getNome() {
+        return nome;
+    }
 
-    public void setNome(String nome) { this.nome = nome; }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-    public Integer getEstoque() { return estoque; }
+    public double getPreco() {
+        return preco;
+    }
 
-    public void setEstoque(Integer estoque) { this.estoque = estoque; }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 
-    public Float getPreco() { return preco; }
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
 
-    public void setPreco(Float preco) { this.preco = preco; }
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public void diminuirEstoque(int quantidade) {
+        if (this.quantidadeEstoque >= quantidade) {
+            this.quantidadeEstoque -= quantidade;
+        } else {
+            throw new IllegalArgumentException("Quantidade em estoque insuficiente.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Produto produto = (Produto) obj;
+        return id != null && id.equals(produto.id);
+    }
 }
